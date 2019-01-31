c7bd729b2d3c1f9e3eef547aafc960b12596e969
bman12three4 <byron.lathi@student.lovett.org>
2019-01-31 11:05:10 -0500

    Add functions for reading data

--- Python/AD7490.py
+++ Python/AD7490.py
@@ -10,7 +10,15 @@ class AD7490(object):
         self.ft232h = FT232H.FT232H()
         # Create a SPI interface from the FT232H using pin 8 (C0) as chip select.
         # Use a clock speed of 3mhz, SPI mode 3, and most significant bit first.
-        self.spi = FT232H.SPI(ft232h, cs=8, max_speed_hz=3000000, mode=3, bitorder=FT232H.MSBFIRST
-        self.spi.write()
+        self.spi = FT232H.SPI(ft232h, cs=8, max_speed_hz=3000000, mode=3, bitorder=FT232H.MSBFIRST)
+    
+    def read(self):
+        #data = 0b1000001100110000 #Basic data word, sets power functions to normal, goes to address 0
+        data = 0x8330
+        self.spi.write(data)
+        return self.spi.read()
+
+    def read(self, int channel):
+        return ((read(self) >> channel) & 0x01)
     
 
