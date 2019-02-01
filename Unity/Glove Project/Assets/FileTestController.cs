ae953d2fd145fb88e141fb607c76f58f82847302
bman12three4 <byron.lathi@student.lovett.org>
2019-01-31 21:57:17 -0500

    Add text controller to test finger

---
+++ Unity/Glove Project/Assets/FileTestController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FileTestController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string[] lines = System.IO.File.ReadAllLines("/home/byron/Desktop/GloveProject/GloveProject/adc.txt");
+        Debug.Log(lines[0]);
+
+        transform.eulerAngles = new Vector3(float.Parse(lines[0]), 0, 0);
+        //transform.eulerAngles = new Vector3(90, 0, 0);
+
+    }
+}
