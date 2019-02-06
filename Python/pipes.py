18003dd2b313317893dc40117bedce2f2fbdcdec
bman12three4 <byron.lathi@student.lovett.org>
2019-02-06 14:23:27 -0500

    added comments

--- Python/pipes.py
+++ Python/pipes.py
@@ -2,19 +2,23 @@ import time, os, errno
 
 class Pipe():
 
+    #Location of the pipe
     PATH = "/tmp/glove"
 
-    def write(self, dList):
-        data = b''
-        for position in range(len(dList)):
-            data += b'%i,' % (dList[position])
-        data += b'\n'
+    
+    #The write funcion writes the values in the array to the pipe with commas delimiting.
 
-        try:
-            pipe = os.open(self.PATH, os.O_WRONLY | os.O_NONBLOCK)
-            os.write(pipe, data)
-            os.close(pipe)
-            time.sleep(0.05)
+    def write(self, dList):
+        data = b''                                  #create the binary string
+        for position in range(len(dList)):          #for each value in the array
+            data += b'%i,' % (dList[position])      #Add that value and a comma to the binary string
+        data += b'\n'                               #At the end of the loop add a newline.
+
+        try:                                        #Open up the pipe in write only, nonblocking mode 
+            pipe = os.open(self.PATH, os.O_WRONLY | os.O_NONBLOCK) 
+            os.write(pipe, data)                    #write the data to the pipe
+            os.close(pipe)                          #close the pipe
+            time.sleep(0.05)                        #sleep for 1/20 seconds. decrease this for faster refresh rate
         except OSError as err:
             if err.errno == 6:
                 pass
