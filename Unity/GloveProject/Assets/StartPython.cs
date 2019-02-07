147e93bb2e9589ca6c6f03d3fc1d3b8b14894c4f
bman12three4 <byron.lathi@student.lovett.org>
2019-02-07 07:23:26 -0500

    remove space from folder name, move python folder into unity folder

---
+++ Unity/GloveProject/Assets/StartPython.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+public class StartPython : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ProcessStartInfo start = new ProcessStartInfo();
+        start.FileName = "/usr/bin/python";
+
+        string path = Application.dataPath + "/Python/main.py";
+
+        UnityEngine.Debug.Log(path);
+
+        //start.Arguments = string.Format("{0} {1}", "/Users/byron.lathi/Desktop/School/GloveProject/GloveProject/Python/main.py", "");
+        start.Arguments = string.Format("{0} {1}", path, "");
+        start.UseShellExecute = true;
+        start.UseShellExecute = true;
+        start.RedirectStandardOutput = false;
+        Process.Start(start);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
