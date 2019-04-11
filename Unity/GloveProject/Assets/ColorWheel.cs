b604b884527e1b5c7ccb0efd4de9da8c6374321f
bman12three4 <byron.lathi@student.lovett.org>
2019-04-11 16:55:38 -0400

    Add new painting scene
    Added Canvas
    
    Added the color wheel and paintbrush (non-functional)

---
+++ Unity/GloveProject/Assets/ColorWheel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorWheel : MonoBehaviour
+{
+
+    bool visible = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if (Input.GetKeyDown(KeyCode.C)){
+             visible = !visible;
+         }
+
+         if (visible){
+             transform.position = new Vector3(-4.3F, -4.2F, 5.0F);
+         } else {
+             transform.position = new Vector3(-4.3F, -4.2F, -15.0F);
+         }
+    }
+
+    void OnCollisionEnter(Collision collision){
+        Debug.Log("Collision");
+        Debug.Log(collision.GetContact(0).point);
+    }
+}
