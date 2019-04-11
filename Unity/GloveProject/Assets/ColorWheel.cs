5e2aa160d1b874e9751eaecee2e43ad598e8ec23
bman12three4 <byron.lathi@student.lovett.org>
2019-04-11 17:27:27 -0400

    Added Collision Triggers

--- Unity/GloveProject/Assets/ColorWheel.cs
+++ Unity/GloveProject/Assets/ColorWheel.cs
@@ -15,19 +15,18 @@ public class ColorWheel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-         if (Input.GetKeyDown(KeyCode.C)){
-             visible = !visible;
-         }
+        if (Input.GetKeyDown(KeyCode.C)){
+            visible = !visible;
 
-         if (visible){
-             transform.position = new Vector3(-4.3F, -4.2F, 5.0F);
-         } else {
-             transform.position = new Vector3(-4.3F, -4.2F, -15.0F);
-         }
+            if (visible){
+               transform.position = new Vector3(-4.3F, -4.2F, 5.0F);
+            } else {
+                transform.position = new Vector3(-4.3F, -4.2F, -15.0F);
+            }
+        }
     }
 
-    void OnCollisionEnter(Collision collision){
-        Debug.Log("Collision");
-        Debug.Log(collision.GetContact(0).point);
+    void OnTriggerEnter(Collider other){
+        Debug.Log(other.transform.position);
     }
 }
